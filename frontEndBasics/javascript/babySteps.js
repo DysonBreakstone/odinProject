ab8cb3ed2153ce55aab17d9ebdaa203d146c426e
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-06-07 14:01:07 -0600

    practice

---
+++ frontEndBasics/javascript/babySteps.js
@@ -0,0 +1,6 @@
+function logHello(){
+  let a = 10
+  let b = 9 * a
+  console.log(7 * a)
+  console.log(b)
+}
---
+++ frontEndBasics/javascript/functions.js
@@ -0,0 +1,15 @@
+function add7(num) {
+  return num + 7
+}
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1, word.length)
+}
+
+function lastLetter(word) {
+  return word[word.length - 1]
+}
